331ec99de663563a0f7b382594c126727fd7a26f
Yakeing <yakeing@gmail.com>
2019-08-07 20:22:09 +0800

    Update php_templateTest.php

--- tests/php_templateTest/php_templateTest.php
+++ tests/php_templateTest/php_templateTest.php
@@ -5,15 +5,17 @@ use php_template\template;
 use PHPUnit\Framework\TestCase;
 class php_templateTest extends TestCase{
   public function testtemplate(){
-    $dir = "/home/www/mould/";
+    $dir = dirname(__FILE__).'/../mould/';
     $tpl = new template($dir);
     $tpl->assign("string","I was the title");
     $tpl->assign("array",array('aaa','bbb','ccc'));
-    $tpl->assign("number",100);
-    $tpl->GetFile("header.html")->GetFile("index.html")->GetFile("footer.html")->render();
-    
-    
-    // undone
-    //$this->assertTrue($set);
+    $tpl->assign("number", date("Y"));
+    $tpl->assign("footer","***, Inc.");
+    $tpl->GetFile("header.tpl")->GetFile("body.tpl")->GetFile("footer.tpl")->render();
+
+    $this->assertContains(date("Y"), $tpl->DiyKeyword);
+    $this->assertFileExists($tpl->CacheDir.'header.tpl');
+    $this->assertFileExists($tpl->CacheDir.'body.tpl');
+    $this->assertFileExists($tpl->CacheDir.'footer.tpl');
   }
 }
